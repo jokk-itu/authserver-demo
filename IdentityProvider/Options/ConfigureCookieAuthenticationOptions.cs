7bad2b8e95ea03ec2dbf0abfddb52d99913d8a41
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-06-16 14:37:14 +0200

    update: cookie options

--- IdentityProvider/Options/ConfigureCookieAuthenticationOptions.cs
+++ IdentityProvider/Options/ConfigureCookieAuthenticationOptions.cs
@@ -1,22 +1,52 @@
-﻿using IdentityProvider.Constants;
+﻿using AuthServer.Core;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
-using CookieBuilder = Microsoft.AspNetCore.Http.CookieBuilder;
 
 namespace IdentityProvider.Options;
 
-public class ConfigureCookieAuthenticationOptions : IConfigureOptions<CookieAuthenticationOptions>
+public class ConfigureCookieAuthenticationOptions : IConfigureNamedOptions<CookieAuthenticationOptions>
 {
-  public void Configure(CookieAuthenticationOptions options)
-  {
-    options.Cookie = new CookieBuilder
-    {
-      Name = CookieConstants.IdentityCookie,
-      HttpOnly = true,
-      SameSite = SameSiteMode.Strict,
-      SecurePolicy = CookieSecurePolicy.Always,
-      MaxAge = TimeSpan.FromDays(2),
-      IsEssential = true
-    };
-  }
+	private readonly IOptionsMonitor<UserInteraction> _userInteractionOptions;
+
+	public ConfigureCookieAuthenticationOptions(
+		IOptionsMonitor<UserInteraction> userInteractionOptions)
+	{
+		_userInteractionOptions = userInteractionOptions;
+	}
+
+	public void Configure(string? name, CookieAuthenticationOptions options)
+	{
+		if (name == IdentityConstants.ApplicationScheme)
+		{
+			// TODO add SessionStore using Garnet
+			
+			options.Cookie.HttpOnly = true;
+			options.Cookie.SameSite = SameSiteMode.Strict;
+			options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+			options.Cookie.IsEssential = true;
+
+			options.SlidingExpiration = true;
+
+			options.LoginPath = _userInteractionOptions.CurrentValue.LoginUri;
+			options.LogoutPath = _userInteractionOptions.CurrentValue.EndSessionUri;
+		}
+
+		if (name == IdentityConstants.ExternalScheme)
+		{
+			// TODO add SessionStore using Garnet
+
+			options.Cookie.HttpOnly = true;
+			options.Cookie.SameSite = SameSiteMode.Strict;
+			options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+			options.Cookie.IsEssential = true;
+
+			options.SlidingExpiration = true;
+		}
+	}
+
+	public void Configure(CookieAuthenticationOptions options)
+	{
+		Configure(null, options);
+	}
 }
